bec0e66dd018a2ce331ac8d2daa08afe1edc9464
Vladimir Dosoudil <dosoudil@redhat.com>
2013-07-15 13:55:24 +0200

    Remove AS4 Security

--- integration/hornetq-jboss-as-integration/src/main/java/org/hornetq/integration/jboss/security/JBossASSecurityManager.java
+++ integration/hornetq-jboss-as-integration/src/main/java/org/hornetq/integration/jboss/security/JBossASSecurityManager.java
@@ -64,8 +64,6 @@ public class JBossASSecurityManager implements HornetQSecurityManager, HornetQCo
 
    private boolean started;
 
-   private boolean isAs5 = true;
-
    private boolean allowClientLogin = false;
 
    private boolean authoriseOnClientLogin = false;
@@ -184,26 +182,12 @@ public class JBossASSecurityManager implements HornetQSecurityManager, HornetQCo
 
    private void popSecurityContext()
    {
-      if (isAs5)
-      {
-         SecurityActions.popSubjectContext();
-      }
-      else
-      {
-         AS4SecurityActions.popSubjectContext();
-      }
+      SecurityActions.popSubjectContext();
    }
 
    private void pushSecurityContext(final SimplePrincipal principal, final char[] passwordChars, final Subject subject)
    {
-      if (isAs5)
-      {
-         SecurityActions.pushSubjectContext(principal, passwordChars, subject, securityDomainName);
-      }
-      else
-      {
-         AS4SecurityActions.pushSubjectContext(principal, passwordChars, subject);
-      }
+      SecurityActions.pushSubjectContext(principal, passwordChars, subject, securityDomainName);
    }
 
    public void addRole(final String user, final String role)
@@ -290,11 +274,6 @@ public class JBossASSecurityManager implements HornetQSecurityManager, HornetQCo
    public void setSecurityDomainName(final String securityDomainName)
    {
       this.securityDomainName = securityDomainName;
-   }
-
-   public void setAs5(final boolean as5)
-   {
-      isAs5 = as5;
    }
 
    public void setAllowClientLogin(final boolean allowClientLogin)
